13169b07c265c8de525b3a2f448ca1322c141d0b
Abondarenko29 <myoblipcoviyzapis@gmail.com>
2022-01-26 17:09:36 +0200

    Шостий

--- GameRunning.py
+++ GameRunning.py
@@ -6,69 +6,61 @@ pg.init ()
 class Area (pg.sprite.Sprite):
     def __init__ (self, x, y, image, speed):
         super ().__init__ ()
-        self.image = pg.transform.scale (pg.image.load (image), (150, 150))
+        self.image = pg.transform.scale (pg.image.load (image), (100, 100))
         self.rect = self.image.get_rect ()
         self.rect.x = x
         self.rect.y = y
         self.speed = speed
-
     def reset (self):
         window.blit (self.image, (self.rect.x, self.rect.y))
 
-    def move_up (self):
-        window.fill ((Beige))
-        self.rect.y -= self.speed
-        window.blit (self.image, (self.rect.x, self.rect.y))
-    def move_down (self):
-        window.fill ((Beige))
-        self.rect.y += self.speed
-        window.blit (self.image, (self.rect.x, self.rect.y))
-    def move_right (self):
-        window.fill ((Beige))
-        self.rect.x += self.speed
-        window.blit (self.image, (self.rect.x, self.rect.y))
-    def move_left (self):
-        window.fill ((Beige))
-        self.rect.x -= self.speed
-        window.blit (self.image, (self.rect.x, self.rect.y))
+class TextArea (Area):
+    def move(self):
+        key = pg.key.get_pressed ()
+        if key[pg.K_LEFT] and self.rect.x >= 0:
+            self.rect.x -= self.speed
+        if key[pg.K_RIGHT] and self.rect.x <= 1700:
+            self.rect.x += self.speed
+        if key[pg.K_UP] and self.rect.y >= 0:
+            self.rect.y -= self.speed
+        if key[pg.K_DOWN] and self.rect.y <= 600:
+            self.rect.y += self.speed
+class Enemy (Area):
+    def move (self):
+        if self.direction == "right":
+            self.rect.x += self.speed
+        if self.direction == "left":
+            self.rect.x -= self.speed
+        if self.rect.x >= 1700:
+            self.direction = "left"
+        if self.rect.x <= 0:
+            self.direction = "right"
 
 window = pg.display.set_mode ((1800, 700))
-pg.display.set_caption ("Доганялки")
-my_profile = Area (0, 0, "object.png", 10)
+pg.display.set_caption ("Лабіринт")
+my_profile = TextArea (0, 0, "object.png", 5)
 x = 0
 y = 0
-enemy = Area (1300, 350, "enemy.png", 15)
+enemy = Enemy (1500, 450, "enemy.png", 20)
+enemy.direction = "left"
 finish = Area (1600, 550, "finish.png", 0)
+enemy.direction = "right"
+clock = pg.time.Clock ()
 '''mixer.music.load ("GameMusic2.ogg")
 mixer.music.play ()'''
 pg.mixer.music.load ("GameMusic.ogg")
 pg.mixer.music.play ()
 clock = pg.time.Clock ()
-charge = pg.key.get_pressed ()
 game = True
 while game:
     window.fill ((Beige))
     my_profile.reset ()
+    my_profile.move ()
+    enemy.move ()
     enemy.reset ()
     finish.reset ()
-    for e in pg.event.get():
+    for e in pg.event.get ():
         if e.type == pg.QUIT:
             game = False
     pg.display.update ()
     clock.tick (60)
-    for i in range (3):
-        enemy.move_right ()
-    for a in range (3):
-        enemy.move_left ()
-
-    if charge[pg.K_DOWN] and y <= 690:
-        my_profile.move_down ()
-
-    if charge[pg.K_UP] and y >= 10:
-        my_profile.move_up ()
-
-    if charge[pg.K_LEFT] and x >= 10:
-        my_profile.move_left ()
-
-    if charge[pg.K_RIGHT] and x <= 1790:
-        my_profile.move_right ()
